006dfbdede163ffa06f842072400c1a82e8e7520
Jonathan Dorety <jonathan.dorety@gmail.com>
2024-04-21 17:06:08 -0400

    Implement add item handler

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
   import InfoButton from "./components/InfoButton.svelte";
   import type { MouseEventHandler } from "svelte/elements";
-  import { lookupItemDescription } from "./lib/helpers";
+  import { hasOwnProperty, lookupItemDescription } from "./lib/helpers";
   import { containers } from "./data/tables";
+  import { ShoppingCart } from "./components/ShoppingCart";
 
-  const items: { containers: Array<Container> } & Record<string, unknown> = {
+  const items: { containers: Array<Container> } = {
     containers,
+  };
+  const cart = new ShoppingCart();
+
+  const addHandler: MouseEventHandler<HTMLButtonElement> = (e) => {
+    try {
+      const [key, indexString] = e.currentTarget.value.split("-");
+      const index = Number(indexString);
+      console.log({ key, indexString, index });
+      if (hasOwnProperty(items, key) && !Number.isNaN(index)) {
+        const table = items[key];
+        if (!Array.isArray(table)) throw new Error("unable to find table");
+        const newItem = table[index];
+        console.log({table, newItem})
+        if (newItem) cart.addItem(newItem, 1);
+        const cartTotal = cart.getCart();
+        console.log("cart", cartTotal);
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const handleInfo: MouseEventHandler<HTMLButtonElement> = (e) => {
@@ -28,7 +49,10 @@
 <main>
   <table>
     <thead
-      ><tr><td>Item</td><td>Cost</td><td>Capacity</td><td>Weight</td></tr
+      ><tr
+        ><td>Item</td><td>Cost (GP)</td><td>Capacity</td><td>Weight</td><td
+          >Quantity</td
+        ></tr
       ></thead
     >
     <tbody
@@ -36,13 +60,17 @@
         <tr
           ><td>{container.name}</td><td>{container.cost}</td><td
             >{container.capacity}</td
-          ><td>{container.weight}</td><td
+          ><td>{container.weight}</td><td><input type="number" /></td><td
+            ><button on:click={addHandler} value={`containers-${index}`}
+              >+</button
+            ></td
+          ><td
             ><InfoButton
               value={`containers-${index}`}
               itemName={container.name}
               handleClick={handleInfo}
             /></td
-          ></tr
+          ><td></td></tr
         >{/each}</tbody
     >
   </table>
@@ -51,5 +79,9 @@
 <style>
   td {
     text-align: left;
+    padding-right: 1rem;
+  }
+  td input {
+    max-width: 2rem;
   }
 </style>
